2f4c4857ddf4af26582a5f7a803f7261d9e9d6dd
eugene <uwm1004@gmail.com>
2022-01-21 20:34:09 +0900

    todolist

---
+++ src/components/ListItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <li>
+        {{text}}
+        <button @click="itemDelete">deleteBtn</button>
+    </li>
+</template>
+<script>
+export default {
+    props:{
+        index:{
+            type: Number
+        },
+        text:{
+         type: String,
+         required: true,
+         default: "hello"
+        }
+    },
+    methods:{
+        itemDelete(){
+            this.$emit('delete', this.index)
+        }
+    }
+}
+</script>
+<style>
+    
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div>
+            <input type="text" v-model="inputValue">
+            <button @click="addItem">add</button>
+        </div>
+        <ul>
+            <ListItem v-for="(value,index) in listArray" 
+            :key="index" 
+            :index="index"
+            :text="value === '' ? undefined : value"
+            @delete="catchEmitProps"
+            />
+        </ul>
+    </div>
+</template>
+
+<script>
+import ListItem from './ListItem.vue'
+
+export default {
+    data(){
+        return{
+            inputValue:"",
+            listArray:[]
+        }
+    },
+    components:{
+        ListItem
+    },
+    methods:{
+        addItem(){
+            this.listArray.push(this.inputValue);
+            this.inputValue = "";
+        },
+        catchEmitProps(index){
+            this.listArray.splice(index,1)//index포함 1개만 없애겠다.
+        }
+    }
+}
+</script>
+<style>
+    
+</style>
